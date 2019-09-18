75c736a02d37a95faf02fe4988f8422fdd60db1d
Ram Putta <ramachandraiah.putta@netenrich.com>
2019-09-18 19:12:01 +0530

    create JGraphTExample Application

---
+++ src/main/java/com/actoneye/graph/JGraphTExampleApplication.java
@@ -0,0 +1,13 @@
+package com.actoneye.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JGraphTExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JGraphTExampleApplication.class, args);
+	}
+
+}
